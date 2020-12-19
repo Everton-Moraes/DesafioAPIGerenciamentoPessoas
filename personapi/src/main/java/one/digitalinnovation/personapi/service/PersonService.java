0c5ee025a62601b8e65275476a08d9aaaf74dc34
Everton <evertongmoraes@gmail.com>
2020-12-19 12:39:34 -0300

    developed person create operation

---
+++ personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(Person person) {
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Created person with ID" + savePerson.getId()).build();
+    }
+}
